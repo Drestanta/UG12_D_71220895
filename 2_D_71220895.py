cf2924381eb667f909f90ee105e52656bad707f7
Drestanta <118432795+Drestanta@users.noreply.github.com>
2022-12-08 16:13:22 +0700

    Add files via upload

---
+++ 2_D_71220895.py
@@ -0,0 +1,17 @@
+# test case 1
+print("Pilihan Model Matematika")
+print("1. Perkalian")
+print("2. Pembagian")
+kalian = int(input("Masukkan model matematika yang diinginkan 1/2 : "))
+angka = int(input("Menampilkan table matematika dari angka: "))
+
+if kalian == 1:
+    for kalian in range(1,11):
+        print(angka, "x", kalian, "=", angka*kalian) 
+elif kalian == 2:
+    for kalian in range(50,66):
+        print(kalian, ":", angka, "=", kalian/angka)
+elif kalian == 3:
+    print("Pilihan tidak tersedia, jangan ngasal!")
+
+
